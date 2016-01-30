14bc3b5a5bca462875d6d0f789a123bce010860b
CircuitMusicLabs <circuit.music.labs@gmail.com>
2016-01-30 09:56:11 +0100

    added windowing functions

---
+++ cmwindows.h
@@ -0,0 +1,7 @@
+#ifndef _cmwindows_h
+#define _cmwindows_h
+
+void cm_hann(int *w_size);
+void cm_rectangular(int *w_size);
+
+#endif
---
+++ cmwindows_functions.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <math.h>
+#include "cmwindows.h"
+
+
+
+// HANN WINDOW
+void cm_hann(double *window, int *w_size) {
+	for (i = 0; i < w_size; i++) {
+		window[i] = 0.5 (1 - cos((2 * M_PI * i) / (w_size - 1)));
+	}
+	return;
+}
+
+// RECTANGULAR WINDOW
+void cm_rectangular(double *window, int *w_size) { 
+	for (i = 0; i < w_size; i++) {
+		window[i] = 1;
+	}
+	return;
+}
