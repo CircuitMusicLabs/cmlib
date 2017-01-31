d1db0fd93bdb9c28b6e0d99c3203d7dfa22db1a8
CircuitMusicLabs <circuit.music.labs@gmail.com>
2017-01-31 18:51:33 +0100

    updated library headers and functions

--- cmwindows.h
+++ cmwindows.h
@@ -1,7 +1,13 @@
 #ifndef _cmwindows_h
 #define _cmwindows_h
 
-void cm_hann(double *window, int *w_size);
-void cm_rectangular(double *window, int *w_size);
+void cm_hann(double *window, long *length);
+void cm_hamming(double *window, long *length);
+void cm_rectangular(double *window, long *length);
+void cm_bartlett(double *window, long *length);
+void cm_flattop(double *window, long *length);
+void cm_gauss2(double *window, long *length);
+void cm_gauss4(double *window, long *length);
+void cm_gauss8(double *window, long *length);
 
 #endif
